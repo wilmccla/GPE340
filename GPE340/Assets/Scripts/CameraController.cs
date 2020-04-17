e278242bf9de830e8c2e6c972b2e7f7e5904405e
William McClay <wilmccla@uat.edu>
2020-04-17 09:59:22 -0500

    Milestone 1 Complete
    Honestly forgot to start a repo until I looked at the rubric lol

---
+++ GPE340/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    public Transform target;
+
+    private Vector3 position;
+
+    void Update()
+    {
+        //Get the position of the target
+        //Lock the Y position to what it currently is
+        //Gradually move towards the target at the given speed
+        position = target.position;
+        position.y = transform.position.y;
+        transform.position = Vector3.MoveTowards(transform.position, position, speed * Time.deltaTime);
+    }
+}
---
+++ GPE340/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField, Tooltip("Speed of Player movement")]
+    private float speed;
+
+    [SerializeField, Tooltip("Speed of Player rotation")]
+    private float rotateSpeed;
+
+    public Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //--------Movement--------
+        //Get input from the X and the Z axis
+        //Give it to the animator to determine which animation to play
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        input *= speed;
+        input = transform.InverseTransformDirection(input);
+        anim.SetFloat("Horizontal", input.x);
+        anim.SetFloat("Vertical", input.z);
+
+        //Spriting
+        //Holding down shift increases the speed to 5 and resets back to 3 once released
+        //Walking/Sprinting animations are automatically determined using the blend tree
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 5f;
+        }
+        else
+        {
+            speed = 3f;
+        }
+
+        //--------Rotation--------
+        //Get a raycast from the mouse
+        //Create a plane and find the point where the raycast intersects the plane
+        //Use quaternions to gradually rotate the character to face the intersection point
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if(plane.Raycast(ray, out distance))
+        {
+            Quaternion rotateTarget = Quaternion.LookRotation(ray.GetPoint(distance) - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTarget, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
